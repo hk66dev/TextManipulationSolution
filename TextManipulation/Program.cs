f5428b6f95b57fa2b78ed47ece5e097b1e21079c
Hans Kronvall <hk66dev@outlook.com>
2022-05-22 17:17:57 +0200

    Multiple changes during development

--- TextManipulation/Program.cs
+++ TextManipulation/Program.cs
@@ -1,8 +1,23 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System.Text;
 using TextManipulationLibrary;
 
-string input = "NA21A/SVESVE02	090909-0909\r\n\r\nNA21A/SVESVE02	 090909-0909 , 080808 0808 \r\nNA21A/SVESVE02\r\nNA21A/SVESVE02 \r\nNA21A/SVESVE02	090909-0909,\r\nNA21A/SVESVE02	090909-0909,080808-0808,070707-0707,060606-0606\r\nNA21B/SVESVE02	100909-0909,080808-0808,070707-0707,060606-0606,050505-0505\r\nNA21B/SVESVE02	110909-0909,080808-0808,070707-0707,060606-0606";
+// separators
+char[] separators = new char[] { ',', ';', '\t' };
 
-ManageText mt = new(input);
+//string input = "NA21A/SVESVE02	090909-0909\r\n\r\nNA21A/SVESVE02	 090909-0909 , 080808 0808 \r\nNA21A/SVESVE02\r\nNA21A/SVESVE02 \r\nNA21A/SVESVE02	090909-0909,\r\nNA21A/SVESVE02	090909-0909,080808-0808,070707-0707,060606-0606\r\nNA21B/SVESVE02	100909-0909,080808-0808,070707-0707,060606-0606,050505-0505\r\nNA21B/SVESVE02	110909-0909,080808-0808,070707-0707,060606-0606";
+StringBuilder sbInput = new ();
+sbInput.Append("NA21A/SVESVE02	010909-0909");
+sbInput.Append("NA21A/SVESVE02	020909-0909");
+sbInput.Append("");
+sbInput.Append("NA21A/SVESVE02	030909 0909");
+sbInput.Append("NA21A/SVESVE02	040909-0909 ");
+sbInput.Append("NA21A/SVESVE02	 050909-0909");
+sbInput.Append("NA21A/SVESVE02	 060909 0909");
+sbInput.Append("NA21A/SVESVE02	 070909 0909 ");
 
-//Console.WriteLine($"Output:{Environment.NewLine}{mt.GetTextOutput()}");
+
+
+ManageText mt = new(sbInput.ToString(), separators);
+
+Console.WriteLine($"Output:{Environment.NewLine}{mt.AddRowToList()}");
--- TextManipulationLibrary/ManageText.cs
+++ TextManipulationLibrary/ManageText.cs
@@ -8,85 +8,143 @@ namespace TextManipulationLibrary
 {
     public class ManageText
     {
-        public string Text { get; }
+        public string InputText { get; }
+        public char[] Separators { get; }
 
-        public ManageText(string text)
+        public ManageText(string inputText, char[] separators)
         {
-            Text = text;
+            InputText = inputText;
+            Separators = separators;
         }
 
-        private List<TextLine> text = new();
-        bool isManySeparatorTypeOfText = false;
+    
+        private List<TextLine> textLines = new();
+        bool isManySeparatorTypeOfInputString = false;
 
         public string AddRowToList()
         {
             //todo
-            foreach (var line in GetNextLine(Text))
+            foreach (var line in GetNextLine(InputText))
             {
                 // split line in two parts
-                string[] col = line.Split(new char[] { ',', ';', '\t' }, 2, StringSplitOptions.TrimEntries);
+                string[] columns = line.Split(Separators, 2, StringSplitOptions.TrimEntries);
 
-                switch (col.Length)
+                switch (columns.Length)
                 {
                     case 0:
                         break;
                     case 1:
-                        col.Append(string.Empty);
+                        columns.Append(string.Empty);
                         break;
                     case 2:
-                        // splitt second part
-                        var s = col[1].Split(new char[] { ',', ';', '\t' }, 2, StringSplitOptions.None);
+                        // splitt second part and check if there is separators
+                        var s = columns[1].Split(Separators, 2, StringSplitOptions.None);
                         // check if second part has separators
                         if (s.Length > 0)
                         {
                             // if so set isManySeparatorTypeOfText = true
-                            isManySeparatorTypeOfText = true;
+                            isManySeparatorTypeOfInputString = true;
                         }
                         break;
                     default:
                         break;
                 }
 
-                // new text line
-                TextLine tl = new(col[0], col[1]);
-                text.Add(tl);
+                // Add new text line to list
+                TextLine tl = new(columns[0], columns[1]);
+                textLines.Add(tl);
             }
 
-            //todo
+
+            // initiate output variable
             string output = string.Empty;
 
-            switch (isManySeparatorTypeOfText)
-            {   case false:
-                    output = GetOneSeparatorTypeOfText();
-                    break;
+            // alter output text depending on number of separators
+            switch (isManySeparatorTypeOfInputString)
+            {
                 case true:
-                    output = GetManySeparatorTypeOfText();
+                    output = ChangeToOneSeparatorTypeOfOutputString();
                     break;
-                default:
+                case false:
+                    output = ChangeToManySeparatorsTypeOfOutputString();
                     break;
             }
 
-
-
-            return Text;
-        }
-
-        private string GetOneSeparatorTypeOfText()
-        {
-            //todo
-            throw new NotImplementedException();
-        }
-
-        private string GetManySeparatorTypeOfText()
-        {
-            throw new NotImplementedException();
+            return output;
         }
 
         /// <summary>
-        /// Read next line
+        /// Change from "One Separator type of string"
+        /// into "Many Separators type of string"
+        /// </summary>
+        /// <returns>string</returns>
+        private string ChangeToManySeparatorsTypeOfOutputString()
+        {
+            //loop thru textLines and build output string
+            var report = new StringBuilder();
+
+            // on each id gather multiple text strings 
+            string currentId = string.Empty;
+            string newString = string.Empty;    
+
+            foreach (TextLine line in textLines)
+            {
+                // if currentId equal to line.Id add text to string
+                if (currentId == line.Id)
+                {
+                    newString += $"{line.Text.Trim()}/t";
+                }
+                else
+                {
+                    // if newString not empty append new line to report
+                    if (newString != string.Empty)
+                    {
+                        // Add line to report
+                        report.Append($"{line.Id.Trim()}\t{newString.Trim()}");
+                    }
+
+                    // update currentId
+                    currentId = line.Id;
+                    // update newString
+                    newString = $"{line.Text.Trim()}/t";
+                }
+            }
+
+            return report.ToString();
+        }
+
+        /// <summary>
+        /// Change from "Many Separators type of string"
+        /// into "One Separator type of string"
+        /// </summary>
+        /// <returns>string</returns>
+        private string ChangeToOneSeparatorTypeOfOutputString()
+        {
+
+            //loop through textLines and build output string
+            var report = new StringBuilder();
+
+            foreach (TextLine line in textLines)
+            {
+                // split text part of textline into array
+                var stringArray = line.Text.Split(Separators, StringSplitOptions.TrimEntries);
+
+                foreach (var s in stringArray)
+                {
+                    // Add line to report
+                    report.Append($"{line.Id.Trim()}\t{s.Trim()}");
+                }
+            }
+
+            // return string
+            return report.ToString();
+        }
+
+        /// <summary>
+        /// Read next text line
         /// </summary>
         /// <param name="text"></param>
-        /// <returns></returns>
+        /// <returns>string</returns>
         internal static IEnumerable<string> GetNextLine(string text)
         {
             using (var reader = new StringReader(text))
