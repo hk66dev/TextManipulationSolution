f725a62b0a061b009da08f6c1f7bd597f935e988
Hans Kronvall <hk66dev@outlook.com>
2022-05-31 21:55:07 +0200

    Removed a bit of garbage code

--- TextManipulation/Program.cs
+++ TextManipulation/Program.cs
@@ -7,40 +7,9 @@ char[] separators = new char[] { ',', ';', '\t' };
 
 //string input = "NA21A/SVESVE02	090909-0909\r\n\r\nNA21A/SVESVE02	 090909-0909 , 080808 0808 \r\nNA21A/SVESVE02\r\nNA21A/SVESVE02 \r\nNA21A/SVESVE02	090909-0909,\r\nNA21A/SVESVE02	090909-0909,080808-0808,070707-0707,060606-0606\r\nNA21B/SVESVE02	100909-0909,080808-0808,070707-0707,060606-0606,050505-0505\r\nNA21B/SVESVE02	110909-0909,080808-0808,070707-0707,060606-0606";
 StringBuilder sbInput = new ();
-//sbInput.Append($"NA21A/SVESVE02	 010909-0909{Environment.NewLine}");
-//sbInput.Append($"NA21A/SVESVE02	 {Environment.NewLine}");
-//sbInput.Append($"BF21A/ENGENG06	 {Environment.NewLine}");
-//sbInput.Append($"NA21A/SVESVE02 {Environment.NewLine}");
-//sbInput.Append($"BF21A/ENGENG06{Environment.NewLine}");
-//sbInput.Append($"\tc{Environment.NewLine}");
-//sbInput.Append($"{Environment.NewLine}");
-//sbInput.Append($"\t{Environment.NewLine}");
-//sbInput.Append($"\td{Environment.NewLine}");
-
-//sbInput.Append($"Hej{Environment.NewLine}");
-//sbInput.Append($"BF21A/ENGENG06 \t020909-0909{Environment.NewLine}");
-//sbInput.Append($"NA21A/SVESVE02 \t 030909 0909 {Environment.NewLine}");
-
-
-//sbInput.Append($"IND2/ENGENG06	 040909-0909  {Environment.NewLine}");
-//sbInput.Append($"BF21A/ENGENG06	 050909-0909	 {Environment.NewLine}");
-//sbInput.Append($"NA21A/SVESVE02 060909 0909{Environment.NewLine}");
-//sbInput.Append($"IND2/ENGENG06	 070909 0909 {Environment.NewLine}");
-//sbInput.Append($"NA21A/SVESVE02	 080909 0909 {Environment.NewLine}");
-//sbInput.Append($"BF21A/ENGENG06	 100909 0909 {Environment.NewLine}");
-//sbInput.Append($"BF21A/ENGENG06	 090909 0909 {Environment.NewLine}");
-//sbInput.Append($"IND2/ENGENG06	 110909 0909 {Environment.NewLine}");
-//sbInput.Append($"BF21A/ENGENG06	 090909 0909 {Environment.NewLine}");
-//sbInput.Append($"IND2/ENGENG06	 120909 0909 {Environment.NewLine}");
-
-//sbInput.Append($"\tc{Environment.NewLine}");
-//sbInput.Append($"BF21A/ENGENG06	 130909 0909 {Environment.NewLine}");
-//sbInput.Append($"\ta{Environment.NewLine}");
 
 sbInput.Append($"SA21A/ENGENG06\t120909-0909,110909-0909,100909-0909 {Environment.NewLine}");
-
 sbInput.Append($"IND2/ENGENG06\t120909-0909,090909-0909,IND2/ENGENG06,080909-0909,070909-0909,110909-0909,100909-0909,090909-0909,080909-0909,070909-0909 {Environment.NewLine}");
-
 sbInput.Append($"NA21A/ENGENG06\t120909-0909,110909-0909,100909-0909 {Environment.NewLine}");
 
 
